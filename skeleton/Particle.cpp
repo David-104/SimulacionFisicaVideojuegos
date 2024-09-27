b2128edc74f3728fbea223e781d1776bfc87fc26
David-104 <dcanelad@ucm.es>
2024-09-27 11:31:14 +0200

    Apartado 4

--- skeleton/Particle.cpp
+++ skeleton/Particle.cpp
@@ -1,6 +1,10 @@
 #include "Particle.h"
 #include <cmath>
-#define TIPO_INTEGRACION = 0;
+
+enum IntegrationModes { EULER, SEMI, VERLET };
+
+constexpr IntegrationModes INTEGRATION_MODE = EULER;
+
 
 Particle::Particle(Vector3D pos, Vector3D vel, Vector3D a, float d) : vel(vel), a(a), d(d)
 {
@@ -18,11 +22,19 @@ Particle::~Particle()
 
 void Particle::Integrate(double t)
 {
-	//euler
-	pose->p = pose->p + Vector3(vel.x, vel.y, vel.z) * t;
-	vel = vel * pow(d, t) + a * t;
 
-	//euler semi-implicito
-	vel = vel * pow(d, t) + a * t;
-	pose->p = pose->p + Vector3(vel.x, vel.y, vel.z) * t;
+	switch (INTEGRATION_MODE)
+	{
+	case EULER:
+		pose->p = pose->p + Vector3(vel.x, vel.y, vel.z) * t;
+		vel = vel * pow(d, t) + a * t;
+		break;
+	case SEMI:
+		vel = vel * pow(d, t) + a * t;
+		pose->p = pose->p + Vector3(vel.x, vel.y, vel.z) * t;
+	case VERLET:
+		break;
+	default:
+		break;
+	}
 }
