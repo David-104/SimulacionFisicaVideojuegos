debd44f84fe36066cb423021546a1a43b51067e1
David-104 <dcanelad@ucm.es>
2024-11-12 15:24:55 +0100

    yo que se esto sigue raro

--- skeleton/ExplosionForceGenerator.cpp
+++ skeleton/ExplosionForceGenerator.cpp
@@ -1,5 +1,7 @@
 #include "ExplosionForceGenerator.h"
 
+#include <iostream>
+
 static const float e = 2.71828;
 
 ExplosionForceGenerator::ExplosionForceGenerator(Vector3 pos, float intensity, float radius, float duration) : ForceGenerator(), pos(pos), intensity(intensity), radius(radius), duration(duration), time(0)
@@ -17,12 +19,15 @@ void ExplosionForceGenerator::UpdateForce(Particle* p, double t)
 		float distance = (particlePos - pos).magnitude();
 
 		if (distance < radius) {
-			force = (intensity / pow(distance, 2)) * (particlePos - pos).getNormalized() * pow(e, time / duration);
+			force = (intensity / pow(distance, 2)) * (particlePos - pos).getNormalized() * pow(e, -time / duration);
+			if (isnan(force.magnitude()))
+				force = Vector3(0);
 		}
 		else
 			force = Vector3(0);
 
 		time += t;
 		p->applyForce(force);
+		std::cout << "{" << force.x << " ," << force.y << " ," << force.z << " }\n";
 	}
 }
